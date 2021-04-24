e7c1453def45cc056fa85347b29e125b1dd30e66
Manjaka <manjaka.rajaonson@gmail.com>
2021-04-24 23:18:15 +0300

    Finished Page de connexion integration

--- src/components/PageConnexion/PageConnexion.js
+++ src/components/PageConnexion/PageConnexion.js
@@ -1,10 +1,46 @@
 import React from "react";
 import "./PageConnexion.scss";
+import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
+import {
+	faKey,
+	faLock,
+	faUnlock,
+	faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function PageConnexion() {
 	return (
 		<section className="page-connexion">
-			<h1>Page de connexion</h1>
+			<div className="window">
+				<div className="head">
+					<h2 className="title-sm title">
+						<Icon icon={faLock} /> Connexion
+					</h2>
+				</div>
+				<div className="separator"></div>
+				<form className="form">
+					<div className="form__group">
+						<div className="icon">
+							<Icon icon={faUser} />
+						</div>
+						<input className="input name" type="text" placeholder="Votre nom" />
+					</div>
+					<div className="form__group">
+						<div className="icon">
+							<Icon icon={faKey} />
+						</div>
+						<input
+							className="input password"
+							type="password"
+							placeholder="Votre mot de passe"
+						/>
+					</div>
+					<button className="submit">
+						<Icon icon={faUnlock} /> Se connecter
+					</button>
+				</form>
+				<p className="result">Connexion result goes here</p>
+			</div>
 		</section>
 	);
 }
